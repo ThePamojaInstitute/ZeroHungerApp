6b9c5101141a064db9478fd31b855cbfe73811f9
Noah Glassford <56001967+noah-glassford@users.noreply.github.com>
2023-04-19 14:06:52 -0400

    Custom Users can now be created
    Basically the same stage of progress as before, but now the user class is much more customizable

--- ZeroHunger/src/backend/backendAPI/backendAPI/admin.py
+++ ZeroHunger/src/backend/backendAPI/backendAPI/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.apps import apps
 
-from .models import BoardPost
+from .models import BoardPost, BasicUser
 
 admin.site.register(BoardPost)
+admin.site.register(BasicUser)
--- ZeroHunger/src/backend/backendAPI/backendAPI/managers.py
+++ ZeroHunger/src/backend/backendAPI/backendAPI/managers.py
@@ -6,8 +6,19 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError(_("Email Must Be Set"))
         email = self.normalize_email(email)
-        username = self.get_by_natural_key(username)
-        user = self.model(email=email, username=username, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
+    
+    def create_superuser(self, email, username, password, **extra_fields):
+       
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        
+        return self.create_user(email, username, password, **extra_fields)
+    
+  
