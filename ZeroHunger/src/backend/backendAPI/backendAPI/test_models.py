b4cb84eaeacd82aeda0a1a5493951151b78945b5
Alexander Tran <alexander.tran@ontariotechu.net>
2023-05-11 14:11:38 -0400

    Created unit tests for models

---
+++ ZeroHunger/src/backend/backendAPI/backendAPI/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from datetime import datetime
+from .models import BoardPost, BasicUser
+
+class TestViews(TestCase):
+    # client = Client()
+
+    #Test board post model creation
+    def test_BoardPost(self):
+        post = BoardPost(_productType='test_type', 
+                         _postedBy=0, 
+                         _postedOn=datetime.now(),
+                         _postCaption='test')
+        
+        self.assertEquals(post._productType, 'test_type')
+        self.assertIsInstance(post._postedBy, int)
+        self.assertEquals(post._postedOn, datetime.now())
+        self.assertEquals(post._postCaption, 'test')
+
+    #Tests basic user model creation
+    def test_BasicUser(self):
+        user = BasicUser(username='test_username', email='test@test.com')
+
+        self.assertEquals(str(user), 'test_username')
+        self.assertEquals(user.email, 'test@test.com')
+        self.assertFalse(user.is_staff) #Change for staff and superusers
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
