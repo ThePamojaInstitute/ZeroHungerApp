bbdc6d39e58b439b7c14a11c61c38abe741f2a9b
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-05-19 13:06:51 -0400

    Added a limit of 5 images and a button to delete an image

--- ZeroHunger/src/screens/RequestFormScreen.tsx
+++ ZeroHunger/src/screens/RequestFormScreen.tsx
@@ -1,13 +1,13 @@
-import React, { useContext, useState, useEffect } from "react";
-import { Platform, NativeSyntheticEvent, TextInputChangeEventData, GestureResponderEvent } from "react-native";
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { Platform, ScrollView, FlatList, ImageBackground, TouchableHighlight } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export const RequestFormScreen = ({ navigation }) => {
-    const [hasGalleryPermission, setHasGalleryPermission] = useState(null)
-    const [image, setImage] = useState("ii");
+    const [images, setImages] = useState([]);
 
-    const pickImage = async () => {
+    const pickImages = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
             aspect: [4, 3],
@@ -15,32 +15,57 @@ export const RequestFormScreen = ({ navigation }) => {
             allowsMultipleSelection: true,
             selectionLimit: 5 // only for ios 14+
         });
-        console.log(result);
 
         if (!result['cancelled']) {
             if (Platform.OS === 'web') {
-                setImage(result['selected'][0].uri);
+                result['selected'].slice(0, 5 - images.length).forEach((img: { uri: string; }) => {
+                    setImages(oldArr => [...oldArr, img.uri])
+                })
             } else if (Platform.OS === 'android') {
-                setImage(result['uri']);
+                if (result['selected']) {
+                    result['selected'].slice(0, 5 - images.length).forEach((img: { uri: string; }) => {
+                        setImages(oldArr => [...oldArr, img.uri])
+                    })
+                } else {
+                    if (images.length < 5) {
+                        setImages(oldArr => [...oldArr, result['uri']])
+                    }
+                }
             }
         }
     };
 
+    const renderItem = ({ item }) => (
+        <ImageBackground source={{ uri: item }} style={styles.Img}>
+            <TouchableHighlight
+                onPress={() => { setImages(images.filter(img => img !== item)) }}>
+                <Ionicons name="close-circle" size={32} color="black" style={styles.topRight} />
+            </TouchableHighlight>
+        </ImageBackground>
+    );
+
     return (
-        <View>
-            <Text>Hello Request</Text>
-            <TouchableOpacity testID="RequestFromNav.Button" style={styles.logOutBtnText} onPress={pickImage}>
+        <ScrollView>
+            <Text>The limit is 5 images</Text>
+            <TouchableOpacity testID="RequestFromNav.Button" style={styles.logOutBtnText} onPress={pickImages}>
                 <Text style={styles.logOutBtn}>Access Camera Roll</Text>
             </TouchableOpacity>
-            {image && <Image style={styles.Img} source={{ uri: image }} />}
-        </View>
+            <View style={{ marginLeft: 20 }}>
+                <FlatList
+                    data={images}
+                    renderItem={renderItem}
+                    horizontal
+                />
+            </View>
+            {images.length == 0 && <Text>No Images</Text>}
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     logOutBtn: {
         title: "Login",
-        width: "7%",
+        width: "25%",
         borderRadius: 25,
         marginTop: 10,
         height: 50,
@@ -56,8 +81,13 @@ const styles = StyleSheet.create({
     Img:
     {
         marginTop: 20,
-        width: '100%',
-        height: '50%',
+        marginLeft: 20,
+        width: 100,
+        height: 100,
         resizeMode: 'cover'
     },
+    topRight: {
+        position: 'absolute',
+        right: 0,
+    }
 })
