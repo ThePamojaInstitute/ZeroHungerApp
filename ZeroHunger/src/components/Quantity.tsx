0e7a1dd9d566c87181f4e7b03b8112a9043262d3
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-05-24 10:51:40 -0400

    Fixed Quantity component bug
    Fixed Quantity component bug that it would work on mobile but not web.

--- ZeroHunger/src/components/Quantity.tsx
+++ ZeroHunger/src/components/Quantity.tsx
@@ -3,24 +3,38 @@ import { useState } from "react";
 
 const Quantity = () => {
     const [count, setCount] = useState(0);
+
     const onPressSub = () => {
         if (count > 0) {
             setCount(prevCount => prevCount - 1);
         }
     }
-    const onPressAdd = () => setCount(prevCount => prevCount + 1);
+    const onPressAdd = () => {
+        if (count <= 99) {
+            setCount(prevCount => prevCount + 1)
+        }
+    }
+
+    const handleChange = (value: string) => {
+        if (parseInt(value) && parseInt(value) <= 100) {
+            setCount(parseInt(value))
+        } else if (!parseInt(value)) {
+            setCount(0)
+        }
+    }
 
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.button} onPress={onPressSub}>
                 <Text style={styles.buttonText}>-</Text>
             </TouchableOpacity>
-            <TextInput 
+            <TextInput
                 style={styles.input}
                 keyboardType='numeric'
-            >
-                <Text>{count}</Text>
-            </TextInput>
+                inputMode="numeric"
+                value={count.toString()}
+                onChangeText={value => handleChange(value)}
+            />
             <TouchableOpacity style={styles.button} onPress={onPressAdd}>
                 <Text style={styles.buttonText}>+</Text>
             </TouchableOpacity>
@@ -50,6 +64,6 @@ const styles = StyleSheet.create({
         padding: 10,
         marginLeft: 10,
         fontSize: 15,
-      },
+    },
 })
 export default Quantity
