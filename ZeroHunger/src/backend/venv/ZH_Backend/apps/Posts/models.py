290d1dafe114f8f209331b72a0e6ac54f951b4b8
Noah Glassford <56001967+noah-glassford@users.noreply.github.com>
2023-05-29 13:05:19 -0400

    got rid of 1 of the requirements because it broke something

--- ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/models.py
+++ ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/models.py
@@ -4,7 +4,7 @@ import datetime
 class BoardPost(models.Model):
     title = models.CharField(max_length=128, default="Untitled")
     images = models.CharField(max_length=256, default="linkToImageDB") #link to images in DB
-    postedOn = models.IntegerField(default=datetime.utcnow()) #Stores time as unix timestamp, defaults to UTC
+    postedOn = models.IntegerField(default=1) #Stores time as unix timestamp, defaults to UTC
     postedBy = models.IntegerField(default=2)#User ID of user who made this post
     description = models.CharField(max_length=1024)
     postType = models.CharField(max_length=1, default="r")
