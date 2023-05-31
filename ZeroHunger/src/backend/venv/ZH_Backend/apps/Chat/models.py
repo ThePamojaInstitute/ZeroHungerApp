9f7f322c06b3408b6583e3ad8b09420c472de297
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-05-31 15:15:44 -0400

    Created a chat app using websockets

--- ZeroHunger/src/backend/venv/ZH_Backend/apps/Chat/models.py
+++ ZeroHunger/src/backend/venv/ZH_Backend/apps/Chat/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from apps.Users.models import BasicUser
+import uuid
 
-# Create your models here.
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    online = models.ManyToManyField(to=BasicUser, blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f"{self.name} ({self.get_online_count()})"
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages"
+    )
+    from_user = models.ForeignKey(
+        BasicUser, on_delete=models.CASCADE, related_name="messages_from_me"
+    )
+    to_user = models.ForeignKey(
+        BasicUser, on_delete=models.CASCADE, related_name="messages_to_me"
+    )
+    content = models.CharField(max_length=512)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"From {self.from_user.username} to {self.to_user.username}: {self.content} [{self.timestamp}]"
