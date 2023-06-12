7d18bbad30e14818487ab525ea55023e18d313d7
Noah Glassford <56001967+noah-glassford@users.noreply.github.com>
2023-06-12 13:01:30 -0400

    applied migrations to database

--- ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/models.py
+++ ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/models.py
@@ -7,7 +7,6 @@ class RequestPost(models.Model):
     postedOn = models.IntegerField(default=1) #Stores time as unix timestamp, defaults to UTC
     postedBy = models.IntegerField()#User ID of user who made this post
     description = models.CharField(max_length=1024, blank=True)
-    postType = models.CharField(max_length=1, default="r")
 
     def __str__(self):
         return self.title
@@ -18,7 +17,6 @@ class OfferPost(models.Model):
     postedOn = models.IntegerField(default=1) #Stores time as unix timestamp, defaults to UTC
     postedBy = models.IntegerField()#User ID of user who made this post
     description = models.CharField(max_length=1024, blank=True)
-    postType = models.CharField(max_length=1, default="o")
 
     def __str__(self):
         return self.title
