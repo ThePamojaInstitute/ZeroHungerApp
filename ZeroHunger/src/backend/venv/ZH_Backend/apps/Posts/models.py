1053c3e8704c5f08b730b0b55d82d91786aada0b
Noah Glassford <56001967+noah-glassford@users.noreply.github.com>
2023-06-29 09:38:06 -0400

    Fixed model defaults and redone migrations

--- ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/models.py
+++ ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/models.py
@@ -6,7 +6,7 @@ class RequestPost(models.Model):
     title = models.CharField(max_length=128, default="Untitled")
     images = models.CharField(max_length=256, blank=True) #link to images in DB
     postedOn = models.IntegerField(default=1) #Stores time as unix timestamp, defaults to UTC
-    postedBy = models.ForeignKey(BasicUser, on_delete=models.CASCADE, related_name="request_post_by")
+    postedBy = models.ForeignKey(BasicUser, on_delete=models.CASCADE, related_name="request_post_by", default=1)
     description = models.CharField(max_length=1024, blank=True)
 
     def __str__(self):
@@ -16,7 +16,7 @@ class OfferPost(models.Model):
     title = models.CharField(max_length=128, default="Untitled")
     images = models.CharField(max_length=256, blank=True) #link to images in DB
     postedOn = models.IntegerField(default=1) #Stores time as unix timestamp, defaults to UTC
-    postedBy = models.ForeignKey(BasicUser, on_delete=models.CASCADE, related_name="offer_post_by")
+    postedBy = models.ForeignKey(BasicUser, on_delete=models.CASCADE, related_name="offer_post_by", default=1)
     description = models.CharField(max_length=1024, blank=True)
     def __str__(self):
         return self.title
