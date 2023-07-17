69a73dfbf9ea05c4eb2339ade15e40b2d4e17fbb
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-07-17 10:28:42 -0400

    Created markAsFulfilled and postsHistory views

--- ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/views.py
+++ ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/views.py
@@ -12,6 +12,50 @@ from .serializers import createOfferSerializer, createRequestSerializer
 from apps.Users.models import BasicUser
 
 
+def decode_token(auth_header):
+    try:
+        return jwt.decode(auth_header, settings.SECRET_KEY)
+    except:
+        return Response("Token invalid or not given", 401)
+    
+def add_username(posts):
+    for post in posts:
+                try:
+                    user_id = post['fields']['postedBy'] 
+                    try:   
+                        user = BasicUser.objects.get(pk=user_id)
+                        post.update({'username': user.username})
+                    except:
+                        posts.remove(post)
+                        pass   
+                except Exception as e:
+                    return Response(e.__str__(), 500)
+                
+def get_posts(posts_type, order_by_newest, page, decoded_token):
+    try:
+        if(posts_type == "r"):
+            if(order_by_newest == 'false'):
+                posts = RequestPost.objects.filter(postedBy__pk=decoded_token['user_id']).all().order_by('postedOn')[page * 5:][:5]
+            else:
+                posts = RequestPost.objects.filter(postedBy__pk=decoded_token['user_id']).all().order_by('-postedOn')[page * 5:][:5]
+        elif(posts_type == "o"):
+            if(order_by_newest == 'false'):
+                posts = OfferPost.objects.filter(postedBy__pk=decoded_token['user_id']).all().order_by('postedOn')[page * 5:][:5]
+            else:
+                posts = OfferPost.objects.filter(postedBy__pk=decoded_token['user_id']).all().order_by('-postedOn')[page * 5:][:5]
+        return posts
+    except Exception as e:
+        return Response(e.__str__(), 500) 
+    
+def get_user_posts(data):
+    try:
+            if(data['data']['postType'] == "r"):
+                return RequestPost.objects.get(pk=data['data']['postId'])
+            elif(data['data']['postType'] == "o"):
+                return OfferPost.objects.get(pk=data['data']['postId'])
+    except:
+        return Response("Post not found", 404)
+
 class createPost(APIView):
     def post(self, request, format=JSONParser):
         if (request.data['postType'] == "r"): 
@@ -27,10 +71,7 @@ class createPost(APIView):
         
 class deletePost(APIView):
      def delete(self,request, format=JSONParser):
-        try:
-            decoded_token = jwt.decode(request.headers['Authorization'], settings.SECRET_KEY)
-        except:
-            return Response("Token invalid or not given", 401)
+        decoded_token = decode_token(request.headers['Authorization'])
         
         try:
             if(request.data['postType'] == "r"):
@@ -60,24 +101,14 @@ class requestPostsForFeed(APIView):
         counter = int(data['postIndex'])
 
         if(request.data['postType'] == "r"):
-            obj = RequestPost.objects.all()[counter:][:5]
+            obj = RequestPost.objects.all().filter(fulfilled=False).order_by('-postedOn')[counter:][:5]
         elif(request.data['postType'] == "o"):
-            obj = OfferPost.objects.all()[counter:][:5]
+            obj = OfferPost.objects.all().filter(fulfilled=False).order_by('-postedOn')[counter:][:5]
         
         data = serializers.serialize('json', obj)
         data = json.loads(data)
 
-        for post in data:
-            try:
-                user_id = post['fields']['postedBy'] 
-                try:   
-                    user = BasicUser.objects.get(pk=user_id)
-                    post.update({'username': user.username})
-                except:
-                    data.remove(post)
-                    pass   
-            except Exception as err:
-                return Response(err.__str__(), 500)
+        add_username(data)
         
         return Response(json.dumps(data), status=201)
      
@@ -88,3 +119,38 @@ class requestPostsForFeed(APIView):
         }
 
         return Response(length, status=200)
+     
+class postsHistory(APIView):
+    def get(self, request):
+            decoded_token = decode_token(request.headers['Authorization'])
+            
+            postsType = request.GET.get('postsType','')
+            orderByNewest = request.GET.get('orderByNewest','')
+            page = int(request.GET.get('page',''))
+
+            posts = get_posts(postsType, orderByNewest, page, decoded_token)
+
+            data = serializers.serialize('json', posts)
+            data = json.loads(data)
+
+            add_username(data)
+
+            return Response(data, status=200)
+
+class markAsFulfilled(APIView):
+     def put(self, request):
+        decoded_token = decode_token(request.data['headers']['Authorization'])
+        
+        obj = get_user_posts(request.data)
+        
+        # if user is the owner of the post
+        if(decoded_token['username'] == obj.postedBy.username):
+            try:
+                obj.fulfilled = True
+                obj.save()
+
+                return Response("Post updated successfully!", 200)
+            except:
+                return Response("Error while updating post", 500)
+        else:
+            return Response("Unauthorized", 401)
