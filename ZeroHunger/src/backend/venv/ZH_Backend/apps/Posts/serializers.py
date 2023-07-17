ec2a970f569810ba236832a0b36511efe9423f53
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-07-17 10:16:16 -0400

    Modified the posts serializers with the updated fields

--- ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/serializers.py
+++ ZeroHunger/src/backend/venv/ZH_Backend/apps/Posts/serializers.py
@@ -3,41 +3,46 @@ from .models import RequestPost, OfferPost
 from apps.Users.models import BasicUser
 from apps.Users.serializers import UserSerializer
 from django.db import models
+from datetime import datetime 
 
 class createRequestSerializer (serializers.ModelSerializer):
     title = serializers.CharField(max_length=128)
     images = serializers.CharField(max_length=256, required=False, allow_blank=True)
-    postedOn = serializers.IntegerField(min_value = 1, max_value= 2147483647) #Checks the time value is within 32 bit integer limit and is not negative
+    postedOn = serializers.DateTimeField(default=datetime.now)
     postedBy = serializers.models.ForeignKey(BasicUser, on_delete=models.CASCADE)
     description = serializers.CharField(max_length=1024, required=False, allow_blank=True)
+    fulfilled = serializers.BooleanField(default=False)
 
     class Meta:
         model=RequestPost
         fields = ['title', 'images', 'postedOn', 'postedBy', 
-                  'description']
+                  'description', 'fulfilled']
     def save(self):
         post=RequestPost(title=self.validated_data['title'],
                        images=self.validated_data['images'],
                        postedOn=self.validated_data['postedOn'],
                        postedBy=self.validated_data['postedBy'],
-                       description=self.validated_data['description'])
+                       description=self.validated_data['description'],
+                       fulfilled=self.validated_data['fulfilled'])
         post.save()
 
 class createOfferSerializer (serializers.ModelSerializer):
     title = serializers.CharField(max_length=128)
     images = serializers.CharField(max_length=256, required=False, allow_blank=True)
-    postedOn = serializers.IntegerField(min_value = 1, max_value= 2147483647) #Checks the time value is within 32 bit integer limit and is not negative
+    postedOn = serializers.DateTimeField(default=datetime.now)
     postedBy = serializers.models.ForeignKey(BasicUser, on_delete=models.CASCADE)
     description = serializers.CharField(max_length=1024, required=False, allow_blank=True)
+    fulfilled = serializers.BooleanField(default=False)
 
     class Meta:
         model=OfferPost
         fields = ['title', 'images', 'postedOn', 'postedBy', 
-                  'description']
+                  'description', 'fulfilled']
     def save(self):
         post=OfferPost(title=self.validated_data['title'],
                        images=self.validated_data['images'],
                        postedOn=self.validated_data['postedOn'],
                        postedBy=self.validated_data['postedBy'],
-                       description=self.validated_data['description'])
+                       description=self.validated_data['description'],
+                       fulfilled=self.validated_data['fulfilled'])
         post.save()
