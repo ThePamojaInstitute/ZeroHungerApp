dc7ed83512e86270ac0878b28b73b7f651896b4a
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-07-21 10:56:21 -0400

    Created food filters component

---
+++ ZeroHunger/src/components/FoodFilters.tsx
@@ -0,0 +1,53 @@
+import { Text, TouchableOpacity, View, Image } from "react-native";
+import styles from "../../styles/components/FoodCategoriesStyleSheet"
+import { Colors } from "../../styles/globalStyleSheet";
+import { ScrollView } from "react-native-gesture-handler";
+
+const FoodFilters = ({ state, setState, foodType, getType }) => {
+    const renderItem = (item: number, state: number[], setState: React.Dispatch<React.SetStateAction<number[]>>) => {
+        return (
+            <View key={item} style={styles.item}>
+                <TouchableOpacity
+                    style={[styles.secondaryBtn,
+                    { backgroundColor: state.includes(item) ? Colors.primary : Colors.primaryLight }]}
+                    onPress={() => {
+                        if (state.includes(item)) {
+                            setState(state.filter((num: number) => num != item))
+                        } else {
+                            setState((oldArray: number[]) => [...oldArray, item])
+                        }
+                    }}>
+                    <Text style={[styles.secondaryBtnLabel,
+                    { color: state.includes(item) ? Colors.offWhite : Colors.dark }]}
+                    >{getType(item)}</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+    return (
+        <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            testID="FoodCategories.container"
+            style={{ flexDirection: 'row' }}
+        >
+            <View style={styles.item}>
+                <TouchableOpacity style={styles.secondaryBtn}>
+                    <Image
+                        source={require('../../assets/Filter.png')}
+                        style={{
+                            resizeMode: 'cover',
+                            width: 16,
+                            height: 14,
+                        }} />
+                </TouchableOpacity>
+            </View>
+            {Object.keys(foodType).map(value => {
+                return renderItem(foodType[value], state, setState)
+            })}
+        </ScrollView>
+    )
+}
+
+export default FoodFilters
