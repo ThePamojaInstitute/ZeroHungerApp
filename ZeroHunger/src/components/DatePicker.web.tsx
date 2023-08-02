4e4e73f27c934c342e89b04c50285feb547345b7
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-08-02 09:46:40 -0400

    Created a custom input for the web's DatePicker

--- ZeroHunger/src/components/DatePicker.web.tsx
+++ ZeroHunger/src/components/DatePicker.web.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import React, { forwardRef, useState } from "react";
+import { View } from "react-native";
 import ReactDatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
+import { Colors } from "../../styles/globalStyleSheet";
+import "react-datepicker/dist/react-datepicker.css";
 
 
-const DatePicker = ({ setNeedBy }) => {
+const DatePicker = ({ setNeedBy, errField }) => {
     const minDate = new Date();
     const maxDate = new Date(minDate);
     maxDate.setMonth(maxDate.getMonth() + 1); // Discuss this
@@ -21,9 +22,45 @@ const DatePicker = ({ setNeedBy }) => {
         setNeedBy(formattedDate)
     }
 
+    const ReactDatePickerInput = forwardRef<
+        HTMLInputElement,
+        React.DetailedHTMLProps<
+            React.InputHTMLAttributes<HTMLInputElement>,
+            HTMLInputElement
+        >
+    >((props, ref) => (
+        <div>
+            <div style={{
+                backgroundColor: Colors.offWhite,
+                display: 'flex',
+                width: 130,
+                height: 33,
+                borderWidth: 5,
+                borderColor: 'red',
+                borderRadius: 10,
+                color: 'black',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginBottom: 15,
+                cursor: 'pointer',
+                marginTop: 5,
+                border: `1px solid ${errField === 'needBy' ? Colors.alert2 : '#D1D1D1'}`
+            }} ref={ref} {...props}>
+                <img style={{ height: 18, width: 19.5, marginLeft: 10, position: 'absolute', left: 0 }} src={require('../../assets/calendar.png')}></img>
+                <p style={{
+                    fontFamily: 'PublicSans_400Regular',
+                    fontSize: 13,
+                    color: '#656565',
+                    marginLeft: 30,
+                }}>{selected ? props.value : 'MM/DD/YYYY'}</p>
+            </div>
+        </div>
+    ))
+
     return (
-        <View testID="DatePickerWeb.container" style={{ marginBottom: 15 }}>
+        <View testID="DatePickerWeb.container">
             <ReactDatePicker
+                customInput={<ReactDatePickerInput />}
                 portalId="id"
                 id="datePicker"
                 selected={date}
@@ -31,7 +68,6 @@ const DatePicker = ({ setNeedBy }) => {
                 minDate={minDate}
                 maxDate={maxDate}
             />
-            <Text testID="DatePickerWeb.date">{selected && `selected: ${date.toLocaleDateString()}`}</Text>
         </View>
     );
 };
