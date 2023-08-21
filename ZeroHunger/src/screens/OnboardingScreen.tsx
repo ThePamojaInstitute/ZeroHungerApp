4abd8a1cb5c52dca7bc2c90cb8435e727960ec2b
Alexander Tran <alexander.tran@ontariotechu.net>
2023-08-21 11:25:57 -0400

    Undo onboarding screen update

--- ZeroHunger/src/screens/OnboardingScreen.tsx
+++ ZeroHunger/src/screens/OnboardingScreen.tsx
@@ -16,14 +16,13 @@ export const OnboardingScreen = ({ navigation }, props: Props) => {
     useEffect(() => {
         async function setData() {
             if (Platform.OS === "web") {
-                // const appData = storage.getString("appLaunched")
-                // if (!appData) {
-                //     // storage.set("appLaunched", "false")
-                // }
-                // else {
-                //     navigation.navigate("LoginScreen")
-                // }
-                navigation.navigate("LoginScreen")
+                const appData = storage.getString("appLaunched")
+                if (!appData) {
+                    storage.set("appLaunched", "false")
+                }
+                else {
+                    navigation.navigate("LoginScreen")
+                }
             }
             else {
                 const appData = await AsyncStorage.getItem("appLaunched")
@@ -48,59 +47,59 @@ export const OnboardingScreen = ({ navigation }, props: Props) => {
     })
 
     return (
-        // <></>
-        <Swiper 
-            controlsProps={{
-                dotsTouchable: true,
-                prevTitle: 'Back',
-                nextTitle: 'Next',
-                prevTitleStyle: styles.backButton,
-                nextTitleStyle: styles.nextButton,
-                dotsWrapperStyle: styles.dots,
-                dotActiveStyle: {backgroundColor: Colors.primary},
-                firstPrevElement: 
-                    <Text style={[globalStyles.H4, styles.backButton, {opacity: 0.5}]}>
-                        Back
-                    </Text>,
-                lastNextElement: 
-                    <View style={styles.continueButton}>
-                        <TouchableOpacity 
-                            style={[globalStyles.defaultBtn, {padding: 18}]}
-                            onPress={() => {if(!props.activeIndex) navigation.navigate("LoginScreen")}}
-                        >
-                            <Text style={globalStyles.defaultBtnLabel}>Continue</Text>
-                        </TouchableOpacity>
-                    </View>
-            }}
-        >
-            <View style={styles.view}>
-                <Image source={require('../../assets/Onboarding1.jpg')} resizeMode="center" style={styles.image}/>
-                <View style={styles.view}>
-                    <Text style={[globalStyles.H1, {paddingBottom: 12}]}>Respond to Requests</Text>
-                    <Text style={styles.text}>
-                        Browse through and fulfill requests made by other users who need help with food.
-                    </Text>
-                </View>
-            </View>
-            <View style={styles.view}>
-                <Image source={require('../../assets/Onboarding2.jpg')} resizeMode="center" style={styles.image}/>
-                <View style={styles.view}>
-                    <Text style={[globalStyles.H1, {paddingBottom: 12}]}>Accept Offers</Text>
-                    <Text style={styles.text}>
-                        Explore and pick-up offers made by other users who want to share food and provide assistance.
-                    </Text>
-                </View>
-            </View>
-            <View style={styles.view}>
-                <Image source={require('../../assets/Onboarding3.jpg')} resizeMode="center" style={styles.image}/>
-                <View style={styles.view}>
-                    <Text style={[globalStyles.H1, {paddingBottom: 12}]}>Make Requests & Offers</Text>
-                    <Text style={styles.text}>
-                        Create your own requests or offers of food assistance.
-                    </Text>
-                </View>
-            </View>
-        </Swiper>
+        <></>
+        // <Swiper 
+        //     controlsProps={{
+        //         dotsTouchable: true,
+        //         prevTitle: 'Back',
+        //         nextTitle: 'Next',
+        //         prevTitleStyle: styles.backButton,
+        //         nextTitleStyle: styles.nextButton,
+        //         dotsWrapperStyle: styles.dots,
+        //         dotActiveStyle: {backgroundColor: Colors.primary},
+        //         firstPrevElement: 
+        //             <Text style={[globalStyles.H4, styles.backButton, {opacity: 0.5}]}>
+        //                 Back
+        //             </Text>,
+        //         lastNextElement: 
+        //             <View style={styles.continueButton}>
+        //                 <TouchableOpacity 
+        //                     style={[globalStyles.defaultBtn, {padding: 18}]}
+        //                     onPress={() => {if(!props.activeIndex) navigation.navigate("LoginScreen")}}
+        //                 >
+        //                     <Text style={globalStyles.defaultBtnLabel}>Continue</Text>
+        //                 </TouchableOpacity>
+        //             </View>
+        //     }}
+        // >
+        //     <View style={styles.view}>
+        //         <Image source={require('../../assets/Onboarding1.png')} resizeMode="center" style={styles.image}/>
+        //         <View style={styles.view}>
+        //             <Text style={[globalStyles.H1, {paddingBottom: 12}]}>Respond to Requests</Text>
+        //             <Text style={styles.text}>
+        //                 Browse through and fulfill requests made by other users who need help with food.
+        //             </Text>
+        //         </View>
+        //     </View>
+        //     <View style={styles.view}>
+        //         <Image source={require('../../assets/Onboarding2.png')} resizeMode="center" style={styles.image}/>
+        //         <View style={styles.view}>
+        //             <Text style={[globalStyles.H1, {paddingBottom: 12}]}>Accept Offers</Text>
+        //             <Text style={styles.text}>
+        //                 Explore and pick-up offers made by other users who want to share food and provide assistance.
+        //             </Text>
+        //         </View>
+        //     </View>
+        //     <View style={styles.view}>
+        //         <Image source={require('../../assets/Onboarding3.png')} resizeMode="center" style={styles.image}/>
+        //         <View style={styles.view}>
+        //             <Text style={[globalStyles.H1, {paddingBottom: 12}]}>Make Requests & Offers</Text>
+        //             <Text style={styles.text}>
+        //                 Create your own requests or offers of food assistance.
+        //             </Text>
+        //         </View>
+        //     </View>
+        // </Swiper>
     )
 }
 
