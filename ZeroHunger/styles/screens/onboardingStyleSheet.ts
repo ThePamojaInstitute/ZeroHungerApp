5e83b05bf918943243c911fa6a97b49c572d33f8
Alexander Tran <alexander.tran@ontariotechu.net>
2023-08-21 21:12:58 -0400

    Update onboarding style

--- ZeroHunger/styles/screens/onboardingStyleSheet.ts
+++ ZeroHunger/styles/screens/onboardingStyleSheet.ts
@@ -4,8 +4,6 @@ import { Colors, Fonts } from "../globalStyleSheet";
 const dim = Dimensions.get('window')
 
 export default StyleSheet.create({
-
-
     view: {
         flex: 1,
         alignContent: "center",
@@ -59,7 +57,31 @@ export default StyleSheet.create({
     },
     continueButton: {
         // padding: 12,
-        marginRight: dim.width * 0.1,
+        marginRight: dim.width * 0.09,
         marginBottom: (dim.height * 0.2) + 18
-    }
+    },
+    defaultBtn: {
+        display: "flex",
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: "center",
+        // width: "90%",
+        paddingLeft: 6, 
+        paddingRight: 6,
+        gap: 10,
+        position: 'relative',
+        marginTop: 25,
+        height: 42,
+        borderRadius: 100,
+        backgroundColor: Colors.primary,
+    },
+    defaultBtnLabel: {
+        fontFamily: Fonts.PublicSans_SemiBold,
+        fontWeight: '600',
+        fontStyle: 'normal',
+        fontSize: 17,
+        display: 'flex',
+        alignItems: 'center',
+        color: Colors.offWhite,
+    },
 })
